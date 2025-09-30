0adf4f61aa904a9164f98790cf970d9cd3018d53
pranai <50417534+Pranaireddy@users.noreply.github.com>
2025-09-30 15:05:49 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,6 @@ sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 sudo mkdir -p /var/tmp_disk
 sudo chmod 1777 /var/tmp_disk
 sudo mount --bind /var/tmp_disk /tmp
@@ -14,3 +13,4 @@ echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
 sudo systemctl mask tmp.mount
 df -h /tmp
 sudo systemctl restart jenkins
+sudo systemctl status jenkins
